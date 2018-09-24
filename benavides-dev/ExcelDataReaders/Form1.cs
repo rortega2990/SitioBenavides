c3de3db0deac99877bafcd96ae03031d8c37de25
rortega2990 <33264446+rortega2990@users.noreply.github.com>
2018-09-24 15:48:45 -0500

    FORM1
    nuevos cambios

--- benavides-dev/ExcelDataReaders/Form1.cs
+++ benavides-dev/ExcelDataReaders/Form1.cs
@@ -33,11 +33,6 @@ namespace ExcelDataReaders
             Dictionary<String, Municipios> cities = new Dictionary<string, Municipios>();
 
 
-            Dictionary<String, int> MiArreglo = new Dictionary<String, int>();
-            MiArreglo.Add("roger", 2);
-
-
-
             StreamReader fileReader = new StreamReader(fileName,Encoding.UTF8);
             while (!fileReader.EndOfStream)
             {
